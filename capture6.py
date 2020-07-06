055919ea370ab174682197bf22671cde5849266e
Vivek Joshi <38205528+joshivivek67@users.noreply.github.com>
2020-07-06 06:45:06 +0530

    Improve

--- capture6.py
+++ capture6.py
@@ -1,16 +1,8 @@
 def palindrome(string):
-    len_of_string= int((len(string))/2)
-    for i in range(0, len_of_string):
-        j = -1
-        k=0
-        if string[k] == string[j]:
-            j -= 1
-            k += 1
-            return str(f"The word {string} is a palindrome")
-        else:
-            return str("not a palindrome")
-
-
+    if string == string[::-1]:
+        return str(f"The Word {string} is Palindrome")
+    else:
+        return str(f"The Word {string} is not Palindrome")
 
 def string_input():
     palindrome_string = input("please input the string: ")
